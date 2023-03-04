3b6537bf5fe8583252b1344842a75c6870d3f785
alicex69 <126929871+alicex69@users.noreply.github.com>
2023-03-03 19:47:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+    document.getElementById("date").innerHTML = Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long', timeZone: 'America/Chicago' }).format(Date.now());
+}, 100);
